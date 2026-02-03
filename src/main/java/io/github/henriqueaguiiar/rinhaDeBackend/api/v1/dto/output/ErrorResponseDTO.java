3d65d406f3ffe89b8c20a68625a4251f6aa36912
Henrique Pacheco <DTI.Henrique.Pacheco@segurossura.com.br>
2026-02-03 16:08:33 -0300

    feat: Adicionado Person Global Exception Handler para tratamento de exceções

---
+++ src/main/java/io/github/henriqueaguiiar/rinhaDeBackend/api/v1/dto/output/ErrorResponseDTO.java
@@ -0,0 +1,37 @@
+package io.github.henriqueaguiiar.rinhaDeBackend.api.v1.dto.output;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(name = "ErrorResponseDTO", description = "Detalhes da resposta de erro retornada pela API.")
+public class ErrorResponseDTO {
+    @Schema(
+            name = "status",
+            description = "Código de status HTTP da resposta de erro.",
+            example = "404 NOT_FOUND")
+    private HttpStatus status;
+    @Schema(
+            name = "message",
+            description = "Mensagem detalhada sobre o erro ocorrido.",
+            example = "Recurso não encontrado.")
+    private String message;
+    @Schema(
+            name = "path",
+            description = "Caminho da requisição que gerou o erro.",
+            example = "/api/v1/pessoas/123")
+    private String path;
+    @Schema(
+            name = "timestamp",
+            description = "Carimbo de data/hora indicando quando o erro ocorreu.",
+            example = "2024-06-15T13:45:30Z")
+    private Instant timestamp;
+
+}
---
+++ src/main/java/io/github/henriqueaguiiar/rinhaDeBackend/api/v1/exception/GlobalRestExceptionHandler.java
@@ -0,0 +1,38 @@
+package io.github.henriqueaguiiar.rinhaDeBackend.api.v1.exception;
+
+
+import io.github.henriqueaguiiar.rinhaDeBackend.api.v1.dto.output.ErrorResponseDTO;
+import io.github.henriqueaguiiar.rinhaDeBackend.domain.exception.CreatePersonException;
+import io.github.henriqueaguiiar.rinhaDeBackend.domain.exception.PersonNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class GlobalRestExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+    @ExceptionHandler(CreatePersonException.class)
+    public ResponseEntity<ErrorResponseDTO> handleCreatePersonException(CreatePersonException exception, HttpServletRequest request) {
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(HttpStatus.BAD_REQUEST,
+                exception.getMessage(),
+                request.getRequestURI() ,
+                Instant.now());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponseDTO);
+    }
+
+    @ExceptionHandler(PersonNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handlePersonNotFoundException(PersonNotFoundException exception, HttpServletRequest request) {
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(HttpStatus.NOT_FOUND,
+                exception.getMessage(),
+                request.getRequestURI(),
+                Instant.now());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponseDTO);
+    }
+
+}
