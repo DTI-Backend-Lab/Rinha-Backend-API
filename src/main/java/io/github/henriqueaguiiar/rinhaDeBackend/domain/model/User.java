bac76ca0f0aa26559904fbdd5b9677668276ec35
Henrique Pacheco <DTI.Henrique.Pacheco@segurossura.com.br>
2026-02-13 12:08:35 -0300

    feat: Adicionado tratamento de erros genericos no GlobalRestExceptionHandler

---
+++ src/main/java/io/github/henriqueaguiiar/rinhaDeBackend/domain/model/User.java
@@ -0,0 +1,11 @@
+package io.github.henriqueaguiiar.rinhaDeBackend.domain.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+
+
+
+}
